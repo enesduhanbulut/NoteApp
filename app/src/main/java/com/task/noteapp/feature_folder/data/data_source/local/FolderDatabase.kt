56754edfde8339b3f0dcd82ea89a1684f2eb2bdf
Duhan <dev.duhanbulut@gmail.com>
2022-05-16 13:39:01 +0300

    fix: fix some files packages

--- app/src/main/java/com/task/noteapp/feature_folder/data/data_source/local/FolderDatabase.kt
+++ app/src/main/java/com/task/noteapp/feature_folder/data/data_source/local/FolderDatabase.kt
@@ -1,4 +1,4 @@
-package com.task.noteapp.feature_note.data.data_source.local
+package com.task.noteapp.feature_folder.data.data_source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
